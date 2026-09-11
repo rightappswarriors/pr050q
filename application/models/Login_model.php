960e0eab371d46d47affae6490f65157be9ecccb
rightappsservices@gmail.com <rightappsservices@gmail.com>
2026-09-11 12:30:21 +0800

    undo 1

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -8,17 +8,7 @@ class Login_model extends CI_Model {
 	}
 	
 	public function check_login($username,$password){
-		
-		$qry = $this->db->query("select a.* from sysusers a where a.username = '$username' and a.password = '".md5($password)."' and a.deleted = 'no' and a.status = 1 ");
-		
-		if ($query->num_rows() === 1) {
-			$user = $query->row_array();
-			print_r($user);
-		} else {
-			var_dump( 'No matching user.');
-		}
-		exit; die();
-		return $qry;
+		return $this->db->query("select a.* from sysusers a where a.username = '$username' and a.password = '".md5($password)."' and a.deleted = 'no' and a.status = 1 ");
 	}
 	
 	public function update_password($id,$data){
